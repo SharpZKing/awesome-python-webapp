de7b4572b598167736fc2a89a65586c11a28359f
zjfsharp <zjfsharp@sina.cn>
2016-08-08 17:29:27 +0800

    modified file: urls.py

--- www/urls.py
+++ www/urls.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
-import logging
+__author__ = 'Michael Liao'
 
-from transwarp.web import get,view
+import os, re, time, base64, hashlib, logging
+
+from transwarp.web import get, post, ctx, view, interceptor, seeother, notfound
+
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 
 from models import User, Blog, Comment
-
-#@view('test_users.html')
-#@get('/')
-#def test_users():
-#	users = User.find_all()
-#	return dict(users = users)
 
 @view('blogs.html')
 @get('/')
 def index():
-	blogs = Blog.find_all()
+    blogs = Blog.find_all()
+    user = User.find_first('where email=?', 'admin@example.com')
+    return dict(blogs=blogs, user=user)
 
-	user = User.find_first('where email=?','admin@example.com')
-	return dict(blogs=blogs,user=user)
-
-	
+@api
+@get('/api/users')
+def api_get_users():
+    users = User.find_by('order by create_at desc')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
